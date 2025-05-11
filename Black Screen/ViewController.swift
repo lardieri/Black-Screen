d6034321167ca5ce9e1c3a4b7e9634b861118127
lardieri <lardieri@users.noreply.github.com>
2025-05-10 19:19:36 -0700

    Initial app

---
+++ Black Screen/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Black Screen
+//
+//  Created by Stephen Lardieri on 2025-05-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        true
+    }
+
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        true
+    }
+}
+
